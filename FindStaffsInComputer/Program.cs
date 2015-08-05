a57d563c91b4822e4ebbdc64c68d83dcb76dad16
Gashaw Zebbo <mggashaw@live.com>
2015-08-05 05:40:25 -0700

    First ChechIn - Create a project

---
+++ FindStaffsInComputer/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FindStaffsInComputer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Start New Cool Projects
+        }
+    }
+}
